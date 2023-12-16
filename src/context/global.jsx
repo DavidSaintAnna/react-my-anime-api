281ed41250791339eb93279d71e527be9199086a
David <david.henri.santana@gmail.com>
2023-12-16 12:13:59 -0300

    update: add fetch and get functions  for HomePage

--- src/context/global.jsx
+++ src/context/global.jsx
@@ -16,6 +16,12 @@ const reducer = (state, action) => {
       return { ...state, loading: true };
     case GET_POPULAR_ANIME:
       return { ...state, popularAnime: action.payload, loading: false };
+    case SEARCH:
+      return { ...state, searchResults: action.payload, loading: false };
+    case GET_UPCOMING_ANIME:
+      return { ...state, upcomingAnime: action.payload, loading: false };
+    case GET_AIRING_ANIME:
+      return { ...state, airingAnime: action.payload, loading: false };
     default:
       return state;
   }
@@ -36,6 +42,25 @@ export const GlobalContextProvider = ({ children }) => {
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [search, setSearch] = React.useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value === "") {
+      state.isSearch = false;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search) {
+      searchAnime(search);
+      state.isSearch = true;
+    } else {
+      state.isSearch = false;
+      alert("Please enter a search term");
+    }
+  };
 
   const getPopularAnime = async () => {
     dispatch({ type: LOADING });
@@ -44,13 +69,48 @@ export const GlobalContextProvider = ({ children }) => {
     dispatch({ type: GET_POPULAR_ANIME, payload: data.data });
   };
 
+  const getUpcomingAnime = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseUrl}/top/anime?filter=upcoming`);
+    const data = await response.json();
+    dispatch({ type: GET_UPCOMING_ANIME, payload: data.data });
+  };
+
+  const getAiringAnime = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseUrl}/top/anime?filter=airing`);
+    const data = await response.json();
+    dispatch({ type: GET_AIRING_ANIME, payload: data.data });
+  };
+
+  const searchAnime = async (anime) => {
+    dispatch({ type: LOADING });
+    const response = await fetch(
+      `${baseUrl}/search/anime?q=${anime}&order_by=  popularity&sort=asc&sfw`
+    );
+    const data = await response.json();
+    dispatch({ type: SEARCH, payload: data.data });
+  };
+
   //initial render
   React.useEffect(() => {
     getPopularAnime();
   }, []);
 
   return (
-    <GlobalContext.Provider value={{ ...state }}>
+    <GlobalContext.Provider
+      value={{
+        ...state,
+        handleChange,
+        handleSubmit,
+        searchAnime,
+        handleChange,
+        search,
+        getPopularAnime,
+        getUpcomingAnime,
+        getAiringAnime,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
