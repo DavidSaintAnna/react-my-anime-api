cfa01eadc6a5df227e223229759c6823e99cfd06
David <david.henri.santana@gmail.com>
2023-12-16 07:10:11 -0300

    update: add card details

--- src/Components/AnimeItem.jsx
+++ src/Components/AnimeItem.jsx
@@ -43,7 +43,68 @@ function AnimeItem() {
           <div className="image">
             <img src={images?.jpg.large_image_url} alt="" />
           </div>
+          <div className="anime-details">
+            <p>
+              <span>Aired:</span>
+              <span>{aired?.string}</span>
+            </p>
+            <p>
+              <span>Rating:</span>
+              <span>{rating}</span>
+            </p>
+            <p>
+              <span>Rank:</span>
+              <span>{rank}</span>
+            </p>
+            <p>
+              <span>Score:</span>
+              <span>{score}</span>
+            </p>
+            <p>
+              <span>Popularity:</span>
+              <span>{popularity}</span>
+            </p>
+            <p>
+              <span>Status:</span>
+              <span>{status}</span>
+            </p>
+            <p>
+              <span>Source:</span>
+              <span>{source}</span>
+            </p>
+            <p>
+              <span>Season:</span>
+              <span>{season}</span>
+            </p>
+            <p>
+              <span>Duration:</span>
+              <span>{duration}</span>
+            </p>
+          </div>
         </div>
+        <p className="description">
+          {showMore ? synopsis : synopsis?.substring(0, 450) + "..."}
+          <button
+            onClick={() => {
+              setShowMore(!showMore);
+            }}
+          >
+            {showMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
+      </div>
+      <h3 className="title">Trailer</h3>
+      <div className="trailer-con">
+        {trailer?.embed_url && (
+          <iframe
+            src={trailer?.embed_url}
+            title="Inline Frame Example"
+            width="800"
+            height="450"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        )}
       </div>
     </div>
   );
