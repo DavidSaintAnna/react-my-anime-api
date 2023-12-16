8576b95b8cd096d9f03f067f4292bd3c00cea29d
David <david.henri.santana@gmail.com>
2023-12-15 22:10:52 -0300

    feat: Created project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Popular from "./Components/Popular";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Popular />
+      </div>
+    </>
+  );
+}
+
+export default App;
