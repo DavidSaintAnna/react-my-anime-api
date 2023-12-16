24819c374aeba5704aef00fac7c11c97c8bf9040
David <david.henri.santana@gmail.com>
2023-12-16 14:30:54 -0300

    update: add gallery router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AnimeItem from "./Components/AnimeItem";
 import Homepage from "./Components/Homepage";
+import Gallery from "./Components/Gallery";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {<Route path="/" element={<Homepage />} />}
+        <Route path="/" element={<Homepage />} />
         <Route path="/anime/:id" element={<AnimeItem />} />
+        <Route path="/character/:id" element={<Gallery />} />
       </Routes>
     </BrowserRouter>
   );
